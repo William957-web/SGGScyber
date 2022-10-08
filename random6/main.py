f142da0aed367c1e2f1972160d99f1f92df313a6
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-10-08 13:15:46 +0800

    people who pro will know what i am doing :D

--- random6/main.py
+++ random6/main.py
@@ -7,3 +7,6 @@ client = pymongo.MongoClient("mongodb+srv://yoni:<password>@cluster0.4b6it9r.mon
 failed = client['failed'].failed
 success = client['success'].success
 proxies = client['proxies'].proxies
+
+while True:
+    r = request.post('http://140.126.151.12/csnskj/permain.asp?CHOICE=OK',data={'txtName':''})
