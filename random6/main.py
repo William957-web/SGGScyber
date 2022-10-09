997eac1fbf6d985a56e08dee0656b323e64e139b
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-10-09 14:05:23 +0800

    not done

--- random6/main.py
+++ random6/main.py
@@ -10,6 +10,7 @@ client = pymongo.MongoClient(os.getenv('db'))
 failed = client['failed'].failed
 success = client['success'].success
 
+
 def run():
     radom = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 6)])
-    result = requests.post('',data={'txtName':''})
+    result = requests.post('http://140.126.151.12/csnskj/Reg_Per.ASP',data={'txtName':os.getenv('name')})
